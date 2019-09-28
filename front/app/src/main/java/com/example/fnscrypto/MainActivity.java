aad0fe5ee66f755913922d521c6f4df2cd1c4f39
s.sharkov <amfitorin@yandex.ru>
2019-09-28 10:42:05 +0300

    first layouts

---
+++ front/app/src/main/java/com/example/fnscrypto/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.fnscrypto;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+    }
+
+
+}
